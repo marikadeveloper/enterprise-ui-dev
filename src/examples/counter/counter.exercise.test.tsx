f220f79cacba3f11a50c538edae458cf30677fcc
Marika Mella <marika.developer@gmail.com>
2024-04-26 20:09:43 +0200

    Add initial count and reset tests

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -1,4 +1,6 @@
-import { screen, render } from '@testing-library/react';
+// @vitest-environment happy-dom
+
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
 
@@ -20,9 +22,19 @@ test('it should increment when the "Increment" button is pressed', async () => {
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', () => {
+  render(<Counter initialCount={5} />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('5');
+});
 
-test.todo(
-  'it should reset the count when the "Reset" button is pressed',
-  async () => {},
-);
+test('it should reset the count when the "Reset" button is pressed', async () => {
+  render(<Counter initialCount={5} />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('5');
+
+  const resetButton = screen.getByRole('button', { name: 'Reset' });
+  await userEvent.click(resetButton);
+
+  expect(currentCount).toHaveTextContent('0');
+});
