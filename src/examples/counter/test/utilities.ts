74c7a92ae4b622244248addd617f615e187d87c4
Marika Mella <marika.developer@gmail.com>
2024-04-26 20:11:06 +0200

    Refactor utilities

--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -2,10 +2,11 @@ import { render as renderComponent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
-export const render = (
-  ui: React.ReactElement,
-  options?: Parameters<typeof renderComponent>[1],
-) => {
+type RenderOptions = Parameters<typeof renderComponent>[1];
+
+export * from '@testing-library/react';
+
+export const render = (ui: React.ReactElement, options?: RenderOptions) => {
   const user = userEvent.setup();
   const result = renderComponent(ui, options);
 
