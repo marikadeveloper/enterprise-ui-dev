24378567518853c39e1ed924fd34e98847b0d6a4
Marika Mella <marika.developer@gmail.com>
2024-04-29 20:33:13 +0200

    Update time-zone component and tests

--- src/examples/time-zone/time-zone.test.tsx
+++ src/examples/time-zone/time-zone.test.tsx
@@ -1,12 +1,21 @@
-import { test, expect, vi } from 'vitest';
 import { render } from 'test/utilities';
+import { afterEach, beforeEach, expect, test, vi } from 'vitest';
 import TimeZone from '.';
+
+beforeEach(() => {
+  vi.useFakeTimers();
+  vi.setSystemTime(new Date('2021-01-01T00:00:00Z'));
+});
 
 test('it should render successfully', () => {
   render(<TimeZone />);
 });
 
-test.fails('should match the snapshot', async () => {
-  const { container } = render(<TimeZone />);
+test('should match the snapshot', async () => {
+  const { container } = render(<TimeZone getTodos />);
   expect(container).toMatchSnapshot();
+});
+
+afterEach(() => {
+  vi.useRealTimers();
 });
